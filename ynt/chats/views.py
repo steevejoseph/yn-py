77d6e067626b4b3bf867d545f1b2e10ad1a9e044
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-11-27 11:14:58 -0500

    Associate chat with user if a user was passed into POST chat call

--- ynt/chats/views.py
+++ ynt/chats/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, Response, request
 
+from type_definitions import User
 from utils import create_response
 from ynt.chats.controllers import add_chat
 from ynt.chats.utils import create_chat_completion
@@ -16,12 +17,15 @@ def handle_chat() -> Response:
 
     content = data.get("content")
     role = data.get("role") or "helpful assistant"
+    user_dict = data.get("user")
+
+    current_user = None if user_dict == None else User(**user_dict)
 
     if content == None:
         return create_response("Please specify 'content' in your json", 400)
 
     completion = create_chat_completion(content, role)
     # print(f"completion: {completion}")
-    completion = add_chat(completion, None)
+    completion = add_chat(completion, current_user)
 
     return create_response("Successfully created chat", 200, data={"chat": completion})
